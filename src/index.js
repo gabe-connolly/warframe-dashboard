a09d901ae5c9f89b266adc1575d2f26543bdaaa5
Gabriel Connolly <gabe@libertyconcepts.com>
2020-12-29 23:30:19 -0500

    WIP with React components.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * Huge credit and thanks to the Warframe community
+ * https://github.com/WFCD/warframe-items
+ */
+const Items = require('warframe-items')
+const React = require('react');
+const ReactDOM = require('react-dom');
+const archwings = new Items({category: ['Archwing']})
+
+console.log(archwings);
+
+const getCDNBase = () => {
+  return 'https://cdn.warframestat.us/img/';
+}
+
+
+class Archwing extends React.Component {
+  render() {
+    return (
+      <div data-name={this.props.name}>
+        {this.props.name}
+        <img src="{getCDNBase() + this.props.imageUrl}"/>
+      </div>
+    )
+  }
+}
+
+
+const Archwings = (props) => {
+    const list = props.items.map(archwing => {
+      return (
+        <Archwing/>
+      )
+    })
+    return list;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Archwings items={archwings}/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+archwings.forEach(archwing => {
+  let elements;
+  for(const key in archwing) {
+    ReactDOM.render(
+      elements,
+      document.getElementById('root')
+    );
+
+    //attr.innerText = `${key}: ${archwing[key]}`;
+  }
+});
