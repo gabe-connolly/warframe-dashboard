c16a81fa8ee94f137e1fe34c0299bb0926252d00
Gabriel Connolly <gabe@libertyconcepts.com>
2021-01-11 10:58:58 -0500

    Updates

---
+++ data/DamageTypes.js
@@ -0,0 +1,55 @@
+// Credit to the Warframe community on Fandom for the data and icons.
+// https://warframe.fandom.com/wiki/Text_Icons
+export default DamageTypes [{
+    'Impact': {
+        'icon_code': '<DT_IMPACT>',
+        'icon_file': 'impact.svg'
+    },
+    'Puncture': {
+        'icon_code': '<DT_PUNCTURE>',
+        'icon_file': 'puncture.svg'
+    },
+    'Slash': {
+        'icon_code': '<DT_SLASH>',
+        'icon_file': 'slash.png'
+    },
+    'Heat': {
+        'icon_code': '<DT_FIRE>',
+        'icon_file': 'heat.webp'
+    },
+    'Cold': {
+        'icon_code': '<DT_FREEZE>',
+        'icon_file': 'cold.png'
+    },
+    'Electricity': {
+        'icon_code': '<DT_ELECTRICITY>',
+        'icon_file': 'electricity.webp'
+    },
+    'Toxin': {
+        'icon_code': '<DT_POISON>',
+        'icon_file': 'toxin_b.webp'
+    },
+    'Blast': {
+        'icon_code': '<DT_POISON>',
+        'icon_file': 'blast_b.webp'
+    },
+    
+
+    ''   <> ' b.png'
+    ''   <DT_EXPLOSION>  ' b.png'
+    'Radiation'   <DT_RADIATION>  'Radiation b.png'
+    'Gas' <DT_GAS>    'Gas b.png'
+    'Magnetic'    <DT_MAGNETIC>   'Magnetic b.png'
+    'Viral'   <DT_VIRAL>  'Viral b.png'
+    'Corrosive'   <DT_CORROSIVE>  'Corrosive b.png'
+    'Void'    <DT_RADIANT>    'VoidTearIcon b.png'
+    'Tau' <DT_SENTIENT>   'SentientFactionIcon b.png'
+    'True'    <DT_FINISHER>   'TrueDmg b.png'
+    'Ionic'   <DT_ELECTRICITY_SPACE>  'Ionic b.png'
+    'Incendiary'  <DT_FIRE_SPACE> 'Incendiary b.png'
+    'Frost'   <DT_FREEZE_SPACE>   'Cold b.png'
+    'Ballistic'   <DT_IMPACT_SPACE>   'Ballistic b.png'
+    'Chem'    <DT_POISON_SPACE>   'Chem b.png'
+    'Plasma'  <DT_PUNCTURE_SPACE> 'Plasma b.png'
+    'Particle'    <DT_SLASH_SPACE>    'Particle b.png'
+}]
