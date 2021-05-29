2f4f15a0b54b9918707e2d89311af5ba5673ef13
Gabriel Connolly <gabe@libertyconcepts.com>
2021-05-29 16:43:54 -0400

    Start extracting sub-categories into their own data file to more accurately represent how their data would be presented in a remote service.

---
+++ src/controllers/subcategories.js
@@ -0,0 +1,83 @@
+export const rarities = [
+    'Common',
+    'Legendary',
+    'Rare',
+    'Uncommon'
+]
+
+export const polarities = [
+    'Madurai',
+    'Naramon',
+    'Penjaga',
+    'Umbra',
+    'Unairu',
+    'Universal',
+    'Vazarin',
+    'Zenurik'
+]
+
+export const modTypes = [
+    '---',
+    'Arcane',
+    'Arch-Gun',
+    'Arch-Melee',
+    'Archwing',
+    'Archwing Mod',
+    'Aura',
+    'Companion Mod',
+    'Creature Mod',
+    'Helminth Charger',
+    'K-Drive Mod',
+    'Kavat',
+    'Kubrow',
+    'Melee',
+    'Melee Mod',
+    'Misc',
+    'Parazon',
+    'Primary',
+    'Rifle',
+    'Secondary',
+    'Secondary Mod',
+    'Sentinel',
+    'Shotgun',
+    'Shotgun Mod',
+    'Stance',
+    'Warframe',
+    'Warframe Mod'
+]
+
+export const primaryWeaponTypes = [
+    'Arm-Cannon',
+    'Bow',
+    'Crossbow',
+    'Glaive',
+    'Launcher',
+    'Pistol',
+    'Rifle',
+    'Shotgun',
+    'Sniper Rifle',
+    'Speargun'
+]
+
+export const noiseLevels = [
+    'Alarming',
+    'Silent'
+]
+
+export const masteryRanks = [
+    0,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15
+]
